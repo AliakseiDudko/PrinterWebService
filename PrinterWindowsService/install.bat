f2bb3fe1c7151f3cc10381bc3cf85a3a1abaf9e9
aleksei.dudko@gmail.com <aleksei.dudko@gmail.com>
2015-09-26 01:45:28 +0300

    User's credentials were added in the installation script under which the service is run.

--- PrinterWindowsService/install.bat
+++ PrinterWindowsService/install.bat
@@ -2,13 +2,15 @@ set SERVICE_NAME="IBoxCorp Printer Service"
 set SERVICE_DESCRIPTION="IBoxCorp Printer Service Description"
 set SERVICE_DISPLAYNAME="IBoxCorp Printer Service Display Name"
 set SERVICE_PATH="%~dp0PrinterWindowsService.exe"
+set SERVICE_ACCOUNT_NAME=".\Admin"
+set SERVICE_ACCOUNT_PASSWORD="Admin"
 
 sc stop %SERVICE_NAME%
 sc delete %SERVICE_NAME%
 
 timeout /t 5
 
-sc create %SERVICE_NAME% displayname= %SERVICE_DISPLAYNAME% binpath= %SERVICE_PATH% start= auto
+sc create %SERVICE_NAME% displayname= %SERVICE_DISPLAYNAME% binpath= %SERVICE_PATH% start= auto obj= %SERVICE_ACCOUNT_NAME% password= %SERVICE_ACCOUNT_PASSWORD%
 sc description %SERVICE_NAME% %SERVICE_DESCRIPTION%
 sc start %SERVICE_NAME%
 
