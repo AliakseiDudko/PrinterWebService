68e14da4aa8e2e1f684a2fbfcc22abdf453acef5
aleksei.dudko@gmail.com <aleksei.dudko@gmail.com>
2015-09-26 22:39:18 +0300

    SERVICE_PATH parameter was removed from installation script.

--- PrinterWindowsService/install.bat
+++ PrinterWindowsService/install.bat
@@ -1,7 +1,6 @@
 set SERVICE_NAME="IBoxCorp Printer Service"
 set SERVICE_DESCRIPTION="IBoxCorp Printer Service Description"
 set SERVICE_DISPLAYNAME="IBoxCorp Printer Service Display Name"
-set SERVICE_PATH="%~dp0PrinterWindowsService.exe"
 set SERVICE_ACCOUNT_NAME=".\Admin"
 set SERVICE_ACCOUNT_PASSWORD="Admin"
 
@@ -10,7 +9,7 @@ sc delete %SERVICE_NAME%
 
 timeout /t 5
 
-sc create %SERVICE_NAME% displayname= %SERVICE_DISPLAYNAME% binpath= %SERVICE_PATH% start= auto obj= %SERVICE_ACCOUNT_NAME% password= %SERVICE_ACCOUNT_PASSWORD%
+sc create %SERVICE_NAME% displayname= %SERVICE_DISPLAYNAME% binpath= "%~dp0PrinterWindowsService.exe" start= auto obj= %SERVICE_ACCOUNT_NAME% password= %SERVICE_ACCOUNT_PASSWORD%
 sc description %SERVICE_NAME% %SERVICE_DESCRIPTION%
 sc start %SERVICE_NAME%
 
